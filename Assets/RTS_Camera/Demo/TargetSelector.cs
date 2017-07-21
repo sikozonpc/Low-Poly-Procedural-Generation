bd5e5821b40cf2a52002784abc786ccfee4e0f90
Tiago Taquelim <sikozonbatata@gmail.com>
2017-07-21 13:07:05 +0100

    Created

---
+++ Assets/RTS_Camera/Demo/TargetSelector.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using RTS_Cam;
+
+[RequireComponent(typeof(RTS_Camera))]
+public class TargetSelector : MonoBehaviour 
+{
+    private RTS_Camera cam;
+    private new Camera camera;
+
+    private void Start()
+    {
+        cam = gameObject.GetComponent<RTS_Camera>();
+        camera = gameObject.GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.CompareTag("Interactable Object"))
+                    cam.SetTarget(hit.transform);
+                else
+                    cam.ResetTarget();
+            }
+        }
+    }
+}
